79fdfbefb81c83454a3f02a6f4a8aa70140893ec
embedded-engg-91 <badigermanoj781@gmail.com>
2026-08-11 19:33:35 +0530

    Add files via upload

---
+++ Source-To-HTML_Manoj_B/test.c
@@ -0,0 +1,30 @@
+/* Multi line comment */
+
+#include <stdio.h>
+
+
+#define MAX 100
+
+const int num = 10;
+float value = 3.14;
+char ch = 'A';
+
+void test(int a)
+{
+    printf("The value of a is: ");
+    if (a > 10)
+    {
+        return;
+    }
+    else
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            break;
+        }
+    }
+    
+
+    printf("Hello World");
+}
+
