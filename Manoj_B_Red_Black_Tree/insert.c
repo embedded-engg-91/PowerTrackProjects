96e52983d464eacb86e65adfd4561e340eeeed27
embedded-engg-91 <badigermanoj781@gmail.com>
2026-08-20 19:38:17 +0530

    Update insert.c

--- Manoj_B_Red_Black_Tree/insert.c
+++ Manoj_B_Red_Black_Tree/insert.c
@@ -1,29 +1,22 @@
-/*******************************************************************************************************************************************************************
-*Title			: Insertion
-*Description		: This function performs inserting the new data into the given Red Black tree.
-*Prototype		: int insert(tree_t **root, data_t data);
-*Input Parameters	: root – Pointer to the root node of the Red Black tree.
-			: data – New data to be inserted into the Red Black tree.
-*Output			: Status (SUCCESS / FAILURE)
-*******************************************************************************************************************************************************************/
+
 #include "rbt.h"
 
 
 int insert(tree_t **root, data_t data)
 {
-	/* Definition goes here */
+	
 	tree_t *node=create_node(data);
 	if(*root == NULL)
 	{
 		*root = node;
-		(*root)->color='B'; //need to update the color cuz  nodes are created with red but root is black 
+		(*root)->color='B'; 
 		return SUCCESS;
 	}
 	else
 	{
 	    
-        /* there are elementss in the tree- do the normal onsert opwrationncuz its a bst only */
-		int flag=0; //to know whether to move left or right
+        
+		int flag=0; 
         tree_t *temp=(*root);
         tree_t *prev=NULL;
         while(temp!=NULL)
@@ -31,14 +24,14 @@ int insert(tree_t **root, data_t data)
             if(data<temp->data)
             {
                 flag=0;
-                prev=temp; //we need na the postion where we need to insert
+                prev=temp; 
 
-                temp=temp->left; //move on the left side 
+                temp=temp->left; 
             }
             else if (data>temp->data)
             {
                 flag=1;
-                prev=temp;//we need na the postion where we need to insert
+                prev=temp;
                 temp=temp->right;
             }
             else 
@@ -49,7 +42,7 @@ int insert(tree_t **root, data_t data)
         if(flag==0)
         {
             prev->left=node;
-            node->parent=prev; //must update the nodes parent as well 
+            node->parent=prev; 
 
         } 
 		else                      
@@ -59,7 +52,7 @@ int insert(tree_t **root, data_t data)
         }
 
 	}
-	//call fix_insert
+	
 	fix_insert(root, &node);
     return SUCCESS;
 }
