8582fb78f42584c1a2642962f999a15a81e0c293
embedded-engg-91 <badigermanoj781@gmail.com>
2026-08-20 19:35:26 +0530

    Update find_max.c
    Removed unnecessary comments and cleaned up code.

--- Manoj_B_Red_Black_Tree/find_max.c
+++ Manoj_B_Red_Black_Tree/find_max.c
@@ -5,8 +5,8 @@ int findmax(tree_t * root)
 
     if(root==NULL) return FAILURE;
 
-    //go on traversing until you reach root->right ==NULL there our max element wil be present
-    //cuz of how data is ORGANSIED in tree
+    
+    
     if(root->right == NULL)
     {
         return root->data;
