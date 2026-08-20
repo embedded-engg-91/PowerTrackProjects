acd9eec120cb0e0051f330ec03412a62dbb8e21e
embedded-engg-91 <badigermanoj781@gmail.com>
2026-08-20 19:37:01 +0530

    Update fix_insert.c

--- Manoj_B_Red_Black_Tree/fix_insert.c
+++ Manoj_B_Red_Black_Tree/fix_insert.c
@@ -3,7 +3,7 @@
 Status fix_insert(tree_t **root, tree_t **node)
 {
 
-    /*find the parent, grandparent, uncle of the node*/
+    
     if (*node == (*root))
         return SUCCESS;
     tree_t *parent = (*node)->parent;
@@ -15,39 +15,39 @@ Status fix_insert(tree_t **root, tree_t **node)
     get_gp_uncle(*node, &uncle, &grandparent);
     if (uncle != NULL && uncle->color == 'R')
     {
-        // recolor
+        
         parent->color = 'B';
         uncle->color = 'B';
         if (grandparent != (*root))
         {
             grandparent->color = 'R';
         }
-        /*otherwise no need to update */
-        fix_insert(root, &grandparent); // needs to continue cuz the granparent might itself cause violation
+        
+        fix_insert(root, &grandparent); 
     }
     else
     {
-        if (grandparent->right == parent && parent->right == (*node)) /* right right rotate case */
+        if (grandparent->right == parent && parent->right == (*node)) 
         {
-            rotate_left(root, &grandparent); // we need to send the granparent of the cuurent node which is grandparent already
+            rotate_left(root, &grandparent); 
             swap_colors(&grandparent, &parent);
         }
 
-        else if (grandparent->left == parent && parent->left == (*node)) /* left left rotate case */
+        else if (grandparent->left == parent && parent->left == (*node)) 
         {
-            rotate_right(root, &grandparent); // we need to send the granparent of the cuurent node which is grandparent already
+            rotate_right(root, &grandparent); 
             swap_colors(&grandparent, &parent);
         }
 
-        else if (grandparent->left == parent && parent->right == (*node)) /* left right rotate case  */
+        else if (grandparent->left == parent && parent->right == (*node)) 
         {
-            /*here we wold need two cases*/
-            rotate_left(root, &parent); // we need to send the granparent of the cuurent node which is grandparent already
-            // swap_colors(&grandparent, &parent); at this point the colore should not be swaapped
-            rotate_right(root, &grandparent); // we need to send the granparent of the cuurent node which is grandparent already
+            
+            rotate_left(root, &parent); 
+            
+            rotate_right(root, &grandparent); 
             swap_colors(&grandparent, node);
         }
-        else /* right left case*/
+        else 
         {
             rotate_right(root, &parent);
             rotate_left(root, &grandparent);
